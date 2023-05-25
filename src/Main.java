c8aac22b6d48f420b843e1ebd448cfaf7db662be
Murad Muradli <muradli.murad96@gmail.com>
2023-05-25 14:22:25 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Wall wall =new Wall(5,6);
+
+        System.out.println("Width "+wall.getHeight());
+        System.out.println("Height "+wall.getWidth());
+        System.out.println("Area "+wall.getArea());
+
+
+    }
+}
